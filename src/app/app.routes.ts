5f7b0d46f9a438de4e2c145e0b5d16b73073fa45
Riyaz J <j.riyazu@gmail.com>
2024-04-12 14:47:42 +0530

    Change user app name and register all user app routes.
    Reference commit ID from user app: 497d24e

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { loadRemoteModule } from "@angular-architects/module-federation";
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "user",
+        loadChildren: () =>
+            loadRemoteModule("user-app", "./User-Routes").then((m) => m.USER_ROUTES),
+    },
+];
